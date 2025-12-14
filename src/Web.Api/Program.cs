03fdb9fdf9eacc95ec716dda98681effac0a8e97
hosein pourtaghi <hosein.pourtaghi@gmail.com>
2025-12-14 20:44:20 +0330

    dev exception

--- src/Web.Api/Program.cs
+++ src/Web.Api/Program.cs
@@ -29,6 +29,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerWithUi();
 
     app.ApplyMigrations();
+    app.UseDeveloperExceptionPage();
 }
 
 app.MapHealthChecks("health", new HealthCheckOptions
