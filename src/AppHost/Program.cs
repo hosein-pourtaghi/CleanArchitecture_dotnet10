cf7083f42a565bb112d137ca7ba714f2364aced4
hosein pourtaghi <hosein.pourtaghi@gmail.com>
2026-01-08 12:37:19 +0330

    fix

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -3,7 +3,7 @@ using Aspire.Hosting;
 var builder = DistributedApplication.CreateBuilder(args);
 
 // Add the web API service
-// var webApi = builder.AddProject<Projects.WebApi>("webapi");
+//var webApi = builder.AddProject<Projects.WebApi>("webapi");
 builder.AddProject("webapi", "../WebApi/WebApi.csproj");
 
 builder.Build().Run();
