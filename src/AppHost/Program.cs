e9342b2611bb807449e12526dac42d9c53067387
hosein pourtaghi <hosein.pourtaghi@gmail.com>
2026-01-08 12:37:55 +0330

    remove

--- src/AppHost/Program.cs
+++
@@ -1,9 +0,0 @@
-using Aspire.Hosting;
-
-var builder = DistributedApplication.CreateBuilder(args);
-
-// Add the web API service
-//var webApi = builder.AddProject<Projects.WebApi>("webapi");
-builder.AddProject("webapi", "../WebApi/WebApi.csproj");
-
-builder.Build().Run();
