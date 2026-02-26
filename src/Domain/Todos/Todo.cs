9e999fecd2a0e6ca6b9adc25350ed0af51262768
hosein pourtaghi <hosein.pourtaghi@gmail.com>
2026-02-26 18:16:06 +0330

    todo entities

---
+++ src/Domain/Todos/Todo.cs
@@ -0,0 +1,21 @@
+﻿using SharedKernel;
+
+namespace Domain.Todos;
+
+public class Todo : Entity
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Priority { get; set; }
+    public DateTime Date { get; set; }
+
+
+
+    #region Navigations 
+    public virtual ICollection<TodoItems> TodoItems { get; set; }
+    #endregion
+
+
+}
---
+++ src/Domain/Todos/TodoItems.cs
@@ -0,0 +1,19 @@
+﻿using SharedKernel;
+
+namespace Domain.Todos;
+
+public class TodoItems : Entity
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; }
+
+    public Guid TodoId{ get; set; }
+
+
+    #region Navigations 
+    public virtual Todo Todo{ get; set; }
+    #endregion
+
+
+}
