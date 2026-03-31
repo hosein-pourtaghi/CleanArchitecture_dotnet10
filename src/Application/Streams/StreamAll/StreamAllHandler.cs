ea6acfafd4344c5851459a33ac297029f53c3741
hosein pourtaghi <hosein.pourtaghi@gmail.com>
2026-03-31 10:07:34 +0330

    stream query

---
+++ src/Application/Streams/StreamAll/StreamAllHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Abstractions.Interfaces;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Streams.StreamAll;
+ 
+ 
+
+/// <summary>
+/// Generic handler for streaming large datasets
+/// </summary>
+public class StreamAllHandler<TEntity, TDto> : IRequestHandler<StreamAllQuery<TEntity, TDto>, IAsyncEnumerable<TDto>>
+    where TEntity : class
+{
+    private readonly IBaseRepository _repository;
+    private readonly IMapper _mapper;
+
+    public StreamAllHandler(IBaseRepository repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+     
+    public async IAsyncEnumerable<TDto> Handle(
+        StreamAllQuery<TEntity, TDto> request,
+      [EnumeratorCancellation]
+    CancellationToken cancellationToken)
+    {
+        var query = _repository.ApplyFiltering(
+            _repository.GetQueryable<TEntity>(),
+            request.Filter);
+
+    
+
+        // Stream results
+        await foreach (var entity in query.AsAsyncEnumerable().WithCancellation(cancellationToken))
+        {
+            var dto = _mapper.Map<TDto>(entity);
+            yield return dto;
+        }
+    }
+     
+}
---
+++ src/Application/Streams/StreamAll/StreamAllQuery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.DTOs.Shared;
+using MediatR;
+
+namespace Application.Streams.StreamAll;
+  
+
+/// <summary>
+/// Generic streaming request for paginated/filtered data
+/// </summary>
+public class StreamAllQuery<TEntity, TDto> :IRequest<IAsyncEnumerable<TDto>>
+      where TEntity : class
+{
+    public PaginatedRequest Filter { get; set; } = new();
+     
+     
+}
